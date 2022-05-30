cc3f8fe7b98ff7fe19b537aaf7322a34fe4fa6ae
RyanKoech <sirryankoech@gmail.com>
2022-05-30 11:38:11 +0300

    Updated bg color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = React.memo(() => {
   };
 
   return (
-    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+    <nav class="bg-slate-50 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
       <div class="container flex flex-wrap justify-between items-center mx-auto">
         <a
           href="https://github.com/RyanKoech/MybroSQL"
