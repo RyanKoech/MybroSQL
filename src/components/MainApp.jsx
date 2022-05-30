4e572dfef6cda93cfbd7264c943711947c048c98
RyanKoech <sirryankoech@gmail.com>
2022-05-30 12:42:18 +0300

    Added Content

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+//Components Imports
+import ColumnCard from "./ColumnCard";
+
 const MainApp = React.memo(() => {
   return (
     <div className="px-2 pt-3 md:pt-20">
-      <h1 className="text-3xl md:text-4xl font-semibold dark:text-slate-100 mb-3">
+      <h1 className="text-3xl sm:text-4xl font-semibold dark:text-slate-100 mb-3">
         Lets Get Started!
       </h1>
       <div className="mb-6">
@@ -19,6 +22,10 @@ const MainApp = React.memo(() => {
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full sm:w-3/12 min-w-[300px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
           required
         />
+      </div>
+      <div>
+        <h2 className="text-xl sm:text-2xl text-gray-800 font-semibold dark:text-slate-300 mb-3">Table Columns</h2>
+        <ColumnCard/>
       </div>
     </div>
   );
