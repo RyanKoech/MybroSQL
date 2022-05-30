25c40b9f3400d014b955f76b2a298a121982f26b
RyanKoech <sirryankoech@gmail.com>
2022-05-30 10:54:52 +0300

    Added functionality to toggle modes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Navbar = () => {
+const Navbar = React.memo(() => {
+  
+  const checkModeIcon = () => {
+    if (
+      localStorage.getItem("color-theme") === "dark" ||
+      (!("color-theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+
+  const [darkMode, setDarkMode] = useState(checkModeIcon());
+
+  const toggleMode = () => {
+    //Toggle Mode
+    setDarkMode((prevMode) => !prevMode);
+
+    // if set via local storage previously
+    if (localStorage.getItem("color-theme")) {
+      if (localStorage.getItem("color-theme") === "light") {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      }
+
+      // if NOT set via local storage previously
+    } else {
+      if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      }
+    }
+  };
+
+  const lightModeIcon = () => {
+    return (
+      <svg
+        id="theme-toggle-light-icon"
+        class="w-5 h-5"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    );
+  };
+
+  const darkModeIcon = () => {
+    return (
+      <svg
+        id="theme-toggle-dark-icon"
+        class="w-5 h-5"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+      </svg>
+    );
+  };
+
   return (
     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
       <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://github.com/RyanKoech/MybroSQL" class="flex items-center" target="_blank">
+        <a
+          href="https://github.com/RyanKoech/MybroSQL"
+          class="flex items-center"
+          target="_blank"
+        >
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             MybroSQL
           </span>
         </a>
         <div class="flex md:order-2">
+          <button
+            id="theme-toggle"
+            type="button"
+            class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-5"
+            onClick={toggleMode}
+          > {darkMode ? lightModeIcon() : darkModeIcon()} </button>
           <a
             href="https://github.com/RyanKoech/MybroSQL"
             type="button"
@@ -62,7 +145,7 @@ const Navbar = () => {
                 href="https://github.com/RyanKoech/MybroSQL"
                 class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Github Repo
+                Visit Repo
               </a>
             </li>
           </ul>
@@ -70,6 +153,6 @@ const Navbar = () => {
       </div>
     </nav>
   );
-};
+});
 
 export default Navbar;
