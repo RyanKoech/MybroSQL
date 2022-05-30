6b4dc428a7b2f03eaf280dd59a1ec87ffd8265f3
RyanKoech <sirryankoech@gmail.com>
2022-05-30 09:15:20 +0300

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+//Components Imports
+import Navbar from './components/Navbar'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="bg-white dark:bg-slate-900">
+      <div className='container min-h-screen mx-auto w-full md:pt-4'>
+        <Navbar/>
+      </div>
     </div>
   )
 }
