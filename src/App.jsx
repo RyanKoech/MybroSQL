806a6ce4b8da378763db33f56a9af1d7e9dac755
RyanKoech <sirryankoech@gmail.com>
2022-05-30 10:52:27 +0300

    Reformatted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 //Components Imports
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="bg-white dark:bg-slate-900">
-      <div className='container min-h-screen mx-auto w-full md:pt-4'>
-        <Navbar/>
+      <div className="container min-h-screen mx-auto w-full md:pt-4">
+        <Navbar />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
