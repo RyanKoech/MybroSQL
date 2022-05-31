d2e3765c6e861f0e18df14a7b175236ab5017190
RyanKoech <sirryankoech@gmail.com>
2022-05-31 07:50:29 +0300

    Added datatypes drop down

--- src/components/ColumnCard.jsx
+++ src/components/ColumnCard.jsx
@@ -9,21 +9,21 @@ const ColumnCard = React.memo(() => {
       <span className="text-base sm:text-lg text-gray-800 dark:text-slate-300">
         &nbsp;:&nbsp;Column Name
       </span>
-      <div className="flex flex-col sm:flex-row sm:items-end mt-3 max-w-[800px] ">
+      <div className="flex flex-col sm:flex-row sm:items-end my-4 max-w-[800px]">
         {/* FOREIGN KEY TOGGLE*/}
         <div className="w-full">
           <label
-            for="foreignKey"
-            class="relative inline-flex items-center mb-4 sm:mb-0 cursor-pointer"
+            htmlFor="foreignKey"
+            className="relative inline-flex items-center mb-4 sm:mb-0 cursor-pointer"
           >
             <input
               type="checkbox"
               value=""
               id="foreignKey"
-              class="sr-only peer"
+              className="sr-only peer"
             />
-            <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
               Foreign Key
             </span>
           </label>
@@ -31,17 +31,17 @@ const ColumnCard = React.memo(() => {
         {/* UNIQUE KEY TOGGLE */}
         <div className="w-full">
           <label
-            for="uniqueKey"
-            class="relative inline-flex items-center mb-4 sm:mb-0 cursor-pointer"
+            htmlFor="uniqueKey"
+            className="relative inline-flex items-center mb-4 sm:mb-0 cursor-pointer"
           >
             <input
               type="checkbox"
               value=""
               id="uniqueKey"
-              class="sr-only peer"
+              className="sr-only peer"
             />
-            <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
               Unique
             </span>
           </label>
@@ -49,7 +49,7 @@ const ColumnCard = React.memo(() => {
         {/* SIZE INPUT  */}
         <div className="w-full">
           <label
-            for="size"
+            htmlFor="size"
             className="inline-block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
           >
             Size
@@ -62,6 +62,77 @@ const ColumnCard = React.memo(() => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 inline-block w-9/12 p-2.5 ml-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
           />
+        </div>
+      </div>
+      <h3 className="inline font-semibold text-lg sm:text-xl text-gray-800 dark:text-slate-300 mb-3">
+        Date Details
+      </h3>
+      <div className="flex flex-col pt-3">
+        <div>
+          <button
+            id="dataTypes"
+            data-dropdown-toggle="dropdown"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="button"
+          >
+            Datatypes{" "}
+            <svg
+              className="w-4 h-4 ml-2"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              ></path>
+            </svg>
+          </button>
+          <div
+            id="dropdown"
+            className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700"
+          >
+            <ul
+              className="py-1 text-sm text-gray-700 dark:text-gray-200"
+              aria-labelledby="dataTypes"
+            >
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Dashboard
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Settings
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Earnings
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Sign out
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
