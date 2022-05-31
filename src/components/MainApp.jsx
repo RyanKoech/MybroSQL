ee610d8b9d552b41fd8c7a038abe30e5fc58187b
RyanKoech <sirryankoech@gmail.com>
2022-05-31 22:10:13 +0300

    Added bottom button

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -24,8 +24,24 @@ const MainApp = React.memo(() => {
         />
       </div>
       <div>
-        <h2 className="text-xl sm:text-2xl text-gray-800 font-semibold dark:text-slate-300 mb-3">Table Columns</h2>
-        <ColumnCard/>
+        <h2 className="text-xl sm:text-2xl text-gray-800 font-semibold dark:text-slate-300 mb-3">
+          Table Columns
+        </h2>
+        <ColumnCard />
+        <div className="flex flex-row-reverse mt-5">
+          <div className="relative">
+            <input
+              type="text"
+              className="z-0 h-10 w-64 pr-36 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 inline-block p-2.5 ml-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Column Name"
+            />
+            <div className="absolute top-0 right-0">
+              <button className="h-10 w-32 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                Add Column
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
