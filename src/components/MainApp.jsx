3d437ed6343e7138701281209a479d3aaee45442
RyanKoech <sirryankoech@gmail.com>
2022-06-02 21:55:12 +0300

    Added conditional rendering

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -4,17 +4,20 @@ import React, { useCallback, useState } from "react";
 import ColumnCard from "./ColumnCard";
 
 const MainApp = React.memo(() => {
-
-  const [enteredCol, setEnteredCol] = useState('');
+  const [enteredCol, setEnteredCol] = useState("");
   const [colNamesList, setColNamesList] = useState([]);
 
-  const addNewColName = useCallback((e) => {
-    e.preventDefault()
+  const addNewColName = useCallback(
+    (e) => {
+      e.preventDefault();
 
-    if(!enteredCol.trim().length || colNamesList.includes(enteredCol) ) return;
+      if (!enteredCol.trim().length || colNamesList.includes(enteredCol))
+        return;
 
-    setColNamesList((prevColList) => [...prevColList, enteredCol])
-  }, [colNamesList, enteredCol]);
+      setColNamesList((prevColList) => [...prevColList, enteredCol]);
+    },
+    [colNamesList, enteredCol]
+  );
 
   return (
     <div className="px-2 pt-3 md:pt-20">
@@ -40,7 +43,15 @@ const MainApp = React.memo(() => {
         <h2 className="text-xl sm:text-2xl text-gray-800 font-semibold dark:text-slate-300 mb-3">
           Table Columns
         </h2>
-        {colNamesList.map((colName) => <ColumnCard key={colName} colName={colName}/>)}
+        {colNamesList.length > 0 ? (
+          colNamesList.map((colName) => (
+            <ColumnCard key={colName} colName={colName} />
+          ))
+        ) : (
+          <div className="mx-auto text-center text-bold text-2xl text-slate-500 bg-slate-50 dark:bg-slate-800 p-2 rounded-lg">
+            No Columns Entered
+          </div>
+        )}
         <div className="flex flex-col sm:flex-row-reverse mt-5 items-end sm:justify-between">
           <div className="relative w-max mb-10 sm:mb-0">
             <input
@@ -48,10 +59,12 @@ const MainApp = React.memo(() => {
               className="z-0 h-10 w-64 pr-36 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 inline-block p-2.5 ml-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Column Name"
               value={enteredCol}
-              onChange={(e) => {setEnteredCol(e.target.value)}}
+              onChange={(e) => {
+                setEnteredCol(e.target.value);
+              }}
             />
             <div className="absolute top-0 right-0">
-              <button 
+              <button
                 className="h-10 w-32 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg rounded-l-none text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 onClick={addNewColName}
               >
