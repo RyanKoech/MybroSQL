276a5490ceb518b3be83bed51855fca4566e279b
RyanKoech <sirryankoech@gmail.com>
2022-06-07 14:21:26 +0300

    Added Logic to Store Column Objects

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,10 +1,15 @@
 import React, {useState, useCallback} from "react";
 
+//Object Imports
+import ColumnObject from "../model/ColumnObject";
+
 export const AppContext = React.createContext();
 
 const AppContextProvider = ({children}) => {
   const [enteredCol, setEnteredCol] = useState("");
   const [colNamesList, setColNamesList] = useState([]);
+  const [colObjList, setColObjList] = useState([]);
+  
 
   const addNewColName = useCallback(
     (e) => {
@@ -14,13 +19,34 @@ const AppContextProvider = ({children}) => {
         return;
 
       setColNamesList((prevColList) => [...prevColList, enteredCol]);
+      setColObjList((prevColObjlist) => [...prevColObjlist, ColumnObject(enteredCol, false, false, 120, "TEXT", "NAME", "")])
+
+      console.log(colObjList);
       setEnteredCol('');
     },
     [colNamesList, enteredCol]
   );
 
+  const updateColObjList = (newObj) => {
+
+    setColObjList( prevColObjlist => {
+
+        const newColObjList = prevColObjlist.map(obj => {
+
+          if(obj.name === newObj.name){
+            return {...obj, ...newObj};
+          } 
+
+          return obj;
+        });
+        
+        console.log(newColObjList);
+        return newColObjList;
+    });
+  };
+
   return (
-    <AppContext.Provider value={{colNamesList, enteredCol, setEnteredCol, addNewColName}}>
+    <AppContext.Provider value={{colNamesList, enteredCol, setEnteredCol, addNewColName, updateColObjList}}>
       {children}
     </AppContext.Provider>
   );
