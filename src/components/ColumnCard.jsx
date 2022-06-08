8f73f65542f08085d0a3822551c62a1fc7da4b0b
RyanKoech <sirryankoech@gmail.com>
2022-06-08 11:37:30 +0300

    Added Logic for toggling foregin key property

--- src/components/ColumnCard.jsx
+++ src/components/ColumnCard.jsx
@@ -2,7 +2,7 @@
 import React, { useContext, useState, useEffect } from "react";
 
 //Component Imports
-import { AppContext } from "../context/AppContext"; 
+import { AppContext } from "../context/AppContext";
 
 //Object Imports
 import ColumnObject from "../model/ColumnObject";
@@ -10,9 +10,8 @@ import ColumnObject from "../model/ColumnObject";
 //Constants Imports
 import { DATA_TYPE, DATA_DOMAINS } from "../model/Constants";
 
-const ColumnCard = React.memo(({colName}) => {
-
-  const {updateColObjList} = useContext(AppContext)
+const ColumnCard = React.memo(({ colName }) => {
+  const { updateColObjList } = useContext(AppContext);
 
   //Information for Generating Column Data
   const [isForeignKey, setIsForeignKey] = useState(false);
@@ -20,34 +19,39 @@ const ColumnCard = React.memo(({colName}) => {
   const [size, setSize] = useState(0);
   const [datatype, setDatatype] = useState(DATA_TYPE.TEXT);
   const [dataDomain, setDataDomain] = useState(DATA_DOMAINS[DATA_TYPE.TEXT][0]);
-  const [customData, setCustomData] = useState('');
+  const [customData, setCustomData] = useState("");
 
   //Loops through DATA_TYPE constant to create options elements list for a select input
   const getDataTypeOptions = () => {
-
     let optionsList = [];
 
-    for(const data_type in DATA_TYPE){
-      const option = <option key={DATA_TYPE[data_type]} value={DATA_TYPE[data_type]}>{DATA_TYPE[data_type]}</option>
-      optionsList.push(option)
+    for (const data_type in DATA_TYPE) {
+      const option = (
+        <option key={DATA_TYPE[data_type]} value={DATA_TYPE[data_type]}>
+          {DATA_TYPE[data_type]}
+        </option>
+      );
+      optionsList.push(option);
     }
 
     return optionsList;
-  }
+  };
 
   //Loops through DATA_DOMAIN[<data_type>] constant to create options elements list for a select input
   const getDataDomainOptions = () => {
-    
     let optionsList = [];
 
     DATA_DOMAINS[datatype].forEach((dataDomain) => {
-      const option = <option key={dataDomain} value={dataDomain}>{dataDomain}</option>
-      optionsList.push(option)
-    })
+      const option = (
+        <option key={dataDomain} value={dataDomain}>
+          {dataDomain}
+        </option>
+      );
+      optionsList.push(option);
+    });
 
     return optionsList;
-
-  }
+  };
 
   return (
     <div className="block p-6 min-w-fit bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mb-5 sm:mb-7">
@@ -89,12 +93,20 @@ const ColumnCard = React.memo(({colName}) => {
               value=""
               id={colName + "_foreignKey"}
               className="sr-only peer"
-              onClick={e => {
-                const _isForeignKey = e.target.checked
+              onClick={(e) => {
+                const _isForeignKey = e.target.checked;
                 updateColObjList(
-                      ColumnObject(colName, _isForeignKey, isUnique, size, datatype, dataDomain, customData)
-                    )
-                setIsForeignKey(_isForeignKey)
+                  ColumnObject(
+                    colName,
+                    _isForeignKey,
+                    isUnique,
+                    size,
+                    datatype,
+                    dataDomain,
+                    customData
+                  )
+                );
+                setIsForeignKey(_isForeignKey);
               }}
             />
             <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
@@ -114,12 +126,20 @@ const ColumnCard = React.memo(({colName}) => {
               value=""
               id={colName + "_uniqueKey"}
               className="sr-only peer"
-              onClick={e => {
-                const _isUnique = e.target.checked
+              onClick={(e) => {
+                const _isUnique = e.target.checked;
                 updateColObjList(
-                  ColumnObject(colName, isForeignKey, _isUnique, size, datatype, dataDomain, customData)
-                )
-                setIsUnique(_isUnique)
+                  ColumnObject(
+                    colName,
+                    isForeignKey,
+                    _isUnique,
+                    size,
+                    datatype,
+                    dataDomain,
+                    customData
+                  )
+                );
+                setIsUnique(_isUnique);
               }}
             />
             <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
@@ -143,12 +163,20 @@ const ColumnCard = React.memo(({colName}) => {
             min="0"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 inline-block w-9/12 p-2.5 ml-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
-            onChange={e => {
-              const _size = e.target.value
+            onChange={(e) => {
+              const _size = e.target.value;
               updateColObjList(
-                ColumnObject(colName, isForeignKey, isUnique, _size, datatype, dataDomain, customData)
-              )
-              setSize(_size)
+                ColumnObject(
+                  colName,
+                  isForeignKey,
+                  isUnique,
+                  _size,
+                  datatype,
+                  dataDomain,
+                  customData
+                )
+              );
+              setSize(_size);
             }}
           />
         </div>
@@ -156,96 +184,123 @@ const ColumnCard = React.memo(({colName}) => {
       <h3 className="inline font-semibold text-lg sm:text-xl text-gray-800 dark:text-slate-300 mb-3">
         Date Details
       </h3>
-      <div className="flex flex-col sm:flex-row sm:items-end pt-3 mb-5">
-        <div className="mb-4 sm:mb-0 sm:mr-10">
-          <label
-            htmlFor="countries"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-          >
-            Datatype
-          </label>
-          <select
-            id="countries"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-8 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            onChange={e => {
-              const _datatype = e.target.value
-              //Update data domain since change in datatype implies change in data domain
-              const _dataDomain = DATA_DOMAINS[_datatype][0]
-              updateColObjList(
-                ColumnObject(colName, isForeignKey, isUnique, size, _datatype, _dataDomain, customData)
-              )
-              setDataDomain(_dataDomain)
-              setDatatype(_datatype)
-            }}
-          >
-            <option defaultValue="">Choose a datatype</option>
-            {getDataTypeOptions()}
-          </select>
-        </div>
-        <div>
-          <label
-            htmlFor="countries"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-          >
-            Data Domain
-          </label>
-          <select
-            id="countries"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-8 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            onChange={e => {
-              const _dataDomain = e.target.value
-              updateColObjList(
-                ColumnObject(colName, isForeignKey, isUnique, size, datatype, _dataDomain, customData)
-              )
-              setDataDomain(_dataDomain)
-            }}
-          >
-            {getDataDomainOptions()}
-          </select>
-        </div>
-      </div>
-      <div>
-        <div className="mb-2 flex flex-row items-end">
-          <label
-            htmlFor="message"
-            className="block text-sm font-medium text-gray-900 dark:text-gray-400 mr-2"
-          >
-            Custom Data
-          </label>
-          <span
-            className="block"
-            title="Enter comma ( , ) separated data to be used for your foreign keys or custom data. NOTE: Datatype and Data domain will be ignored if set"
-          >
-            <svg
-              className="w-4 h-4 text-gray-600 dark:text-slate-400"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+      {!isForeignKey ? (
+        <div className="flex flex-col sm:flex-row sm:items-end pt-3 mb-5">
+          <div className="mb-4 sm:mb-0 sm:mr-10">
+            <label
+              htmlFor="countries"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
-          </span>
+              Datatype
+            </label>
+            <select
+              id="countries"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-8 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              onChange={(e) => {
+                const _datatype = e.target.value;
+                //Update data domain since change in datatype implies change in data domain
+                const _dataDomain = DATA_DOMAINS[_datatype][0];
+                updateColObjList(
+                  ColumnObject(
+                    colName,
+                    isForeignKey,
+                    isUnique,
+                    size,
+                    _datatype,
+                    _dataDomain,
+                    customData
+                  )
+                );
+                setDataDomain(_dataDomain);
+                setDatatype(_datatype);
+              }}
+            >
+              <option defaultValue="">Choose a datatype</option>
+              {getDataTypeOptions()}
+            </select>
+          </div>
+          <div>
+            <label
+              htmlFor="countries"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >
+              Data Domain
+            </label>
+            <select
+              id="countries"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-8 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              onChange={(e) => {
+                const _dataDomain = e.target.value;
+                updateColObjList(
+                  ColumnObject(
+                    colName,
+                    isForeignKey,
+                    isUnique,
+                    size,
+                    datatype,
+                    _dataDomain,
+                    customData
+                  )
+                );
+                setDataDomain(_dataDomain);
+              }}
+            >
+              {getDataDomainOptions()}
+            </select>
+          </div>
         </div>
-        <textarea
-          id="message"
-          rows="4"
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Your custom data..."
-          onChange={e => {
-            const _customData = e.target.value
-            updateColObjList(
-              ColumnObject(colName, isForeignKey, isUnique, size, datatype, dataDomain, _customData)
-            )
-            setCustomData(_customData)
-          }}
-        ></textarea>
-      </div>
+      ) : (
+        <div className="mt-3">
+          <div className="mb-2 flex flex-row items-end">
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-gray-900 dark:text-gray-400 mr-2"
+            >
+              Custom Data
+            </label>
+            <span
+              className="block"
+              title="Enter comma ( , ) separated data to be used for your foreign keys or custom data. NOTE: Datatype and Data domain will be ignored if set"
+            >
+              <svg
+                className="w-4 h-4 text-gray-600 dark:text-slate-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+            </span>
+          </div>
+          <textarea
+            id="message"
+            rows="4"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Your custom data..."
+            onChange={(e) => {
+              const _customData = e.target.value;
+              updateColObjList(
+                ColumnObject(
+                  colName,
+                  isForeignKey,
+                  isUnique,
+                  size,
+                  datatype,
+                  dataDomain,
+                  _customData
+                )
+              );
+              setCustomData(_customData);
+            }}
+          ></textarea>
+        </div>
+      )}
     </div>
   );
 });
