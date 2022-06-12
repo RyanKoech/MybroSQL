4c6ab44081ca273c75ce9fb33221bebccc963a66
RyanKoech <sirryankoech@gmail.com>
2022-06-12 12:37:57 +0300

    Updated Constants

--- src/model/Constants.jsx
+++ src/model/Constants.jsx
@@ -1,3 +1,15 @@
+//Data Domain Constants
+//FOR TEXT
+const NAME = "NAME";
+const WORD = "WORD";
+//FOR INT
+const RANDOM_NUMBER = "RANDOM NUMBER";
+const PHONE = "PHONE";
+//FOR VARCHAR
+const ADDRESS = "ADDRESS";
+const EMAIL = "EMAIL";
+
+
 export const DATA_TYPE = {
   TEXT : "TEXT",
   INT : "INT",
@@ -7,15 +19,15 @@ export const DATA_TYPE = {
 //Maps datatype to specific data domains
 export const DATA_DOMAINS = {
     [DATA_TYPE.TEXT]: [
-      "NAME",
-      "WORD"
+      NAME,
+      WORD
     ],
     [DATA_TYPE.INT] : [
-      "RANDOM NUMBER",
-      "PHONE"
+      PHONE,
+      RANDOM_NUMBER
     ],
     [DATA_TYPE.VARCHAR] : [
-      "ADDRESS",
-      "EMAIL"
+      ADDRESS,
+      EMAIL
     ]
 }
