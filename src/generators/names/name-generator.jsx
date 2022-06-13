7db87a1238a1a79ad562b3b5c62e684be89ea9f9
RyanKoech <sirryankoech@gmail.com>
2022-06-13 09:20:53 +0300

    Improved Logic

--- src/generators/names/name-generator.jsx
+++ src/generators/names/name-generator.jsx
@@ -2,18 +2,38 @@
 import { names } from "./names";
 
 //Used to generate a list of full names
-export const generateFullNames = (rowCount) => {
+export const generateFullNames = (rowCount, isUnique) => {
 
   const fullNameList = []
 
-  for(let i = 0; i < rowCount; i++){
-    const firstname = names[Math.floor(Math.random()*names.length)];
-    const lastname = names[Math.floor(Math.random()*names.length)];
+  // Generates Unique Full Names
+  if(isUnique){
+    for(let i = 0; i < rowCount; i++){
+      let fullName = ""
+      let regen = true;
+      while (regen){
+        const firstName = names[Math.floor(Math.random()*names.length)];
+        const lastName = names[Math.floor(Math.random()*names.length)];
+  
+        fullName = `${firstName} ${lastName}`
 
-    const fullname = `${firstname} ${lastname}`
+        //Keeps generating until a unique full name is obtained
+        regen = fullNameList.includes(fullName);
+      }
+      fullNameList.push(fullName)
+    }
 
-    fullNameList.push(fullname)
+  //Generates None Unique Full Names
+  }else {
+    for(let i = 0; i < rowCount; i++){
+      const firstname = names[Math.floor(Math.random()*names.length)];
+      const lastname = names[Math.floor(Math.random()*names.length)];
+
+      const fullName = `${firstname} ${lastname}`
+
+      fullNameList.push(fullName)
+    }
+
   }
-
   return fullNameList
 }
