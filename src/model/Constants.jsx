e8cad607e51a1418acc8278fafb319e910d89718
RyanKoech <sirryankoech@gmail.com>
2022-06-14 11:22:27 +0300

    Updated data-type and data-domain list

--- src/model/Constants.jsx
+++ src/model/Constants.jsx
@@ -2,32 +2,65 @@
 //FOR TEXT
 export const NAME = "NAME";
 export const WORD = "WORD";
+export const LETTER = "LETTER";
+export const SENTENCE = "SENTENCE";
+export const PARAGRAPHS = 'PARAGRAPHS';
+export const GENDER = 'GENDER';
+export const COMPANY = "COMPANY";
+export const CITY = "CITY";
+export const COUNTRY = "COUNTRY"
+export const STATE = "STATE";
 //FOR INT
 export const RANDOM_NUMBER = "RANDOM NUMBER";
-export const PHONE = "PHONE";
 //FOR VARCHAR
+export const PHONE = "PHONE";
 export const ADDRESS = "ADDRESS";
 export const EMAIL = "EMAIL";
-
+export const URL = 'URL';
+export const POSTAL_CODE = "POSTAL CODE";
+//FOR DATE-TIME
+export const DATE = "DATE";
+export const TIME = "TIME";
+export const DATE_TIME = "DATE-TIME";
+export const YEAR = "YEAR";
+export const TIMESTAMP = "TIMESTAMP";
 
 export const DATA_TYPE = {
   TEXT : "TEXT",
   INT : "INT",
-  VARCHAR : "VARCHAR"
+  VARCHAR : "VARCHAR",
+  DATE_TIME : "DATE-TIME"
 }
 
 //Maps datatype to specific data domains
 export const DATA_DOMAINS = {
     [DATA_TYPE.TEXT]: [
       NAME,
-      WORD
+      WORD,
+      LETTER,
+      SENTENCE,
+      PARAGRAPHS,
+      GENDER,
+      COMPANY,
+      CITY,
+      COUNTRY,
+      STATE
     ],
     [DATA_TYPE.INT] : [
-      PHONE,
       RANDOM_NUMBER
     ],
     [DATA_TYPE.VARCHAR] : [
+      PHONE,
       ADDRESS,
-      EMAIL
+      EMAIL,
+      URL,
+      POSTAL_CODE
+    ],
+    [DATA_TYPE.DATE_TIME] : [
+      DATE,
+      TIME,
+      DATE_TIME,
+      YEAR,
+      TIMESTAMP
     ]
 }
