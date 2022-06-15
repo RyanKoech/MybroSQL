0384e65c1ca3442673d4028ad32d58c229a4bcd0
RyanKoech <sirryankoech@gmail.com>
2022-06-15 09:55:21 +0300

    Added Generator for Time

--- src/generators/date-time/date-time-generator.jsx
+++ src/generators/date-time/date-time-generator.jsx
@@ -13,7 +13,23 @@ const dateTimeGenerator = {
     }
 
     return dateList;
-  }
+  },
+
+  time: (rowCount) => {
+    const timeList = [];
+
+    for(let  i = 0; i < rowCount; i++){
+
+      const dateTime = faker.date.between('1900-01-01T00:00:00.000Z', '1900-01-02T00:00:00.000Z');
+      const time = dateTime.slice(11, 8);
+      timeList.push(time);
+
+    }
+
+    return timeList;
+  },
+
+  
 }
 
 export default dateTimeGenerator;
