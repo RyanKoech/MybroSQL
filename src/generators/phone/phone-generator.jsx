178bcd97a358198fcfa270b3c7576f9e7d211062
RyanKoech <sirryankoech@gmail.com>
2022-06-18 09:33:11 +0300

    Added Infinity loop safety

--- src/generators/phone/phone-generator.jsx
+++ src/generators/phone/phone-generator.jsx
@@ -1,4 +1,8 @@
+//Utilities Imports
 import { reverseString } from "../../utilities/utilities";
+
+//Constants Imports
+import { MAX_TRIES } from "../../model/Constants";
 
 const phoneGenerator = {
   phone :  (rowCount) => {
@@ -7,15 +11,18 @@ const phoneGenerator = {
   
     for(let i = 0; i < rowCount; i++){
       let phoneNumber = "";
+      let maxTries = MAX_TRIES;
       let regen = true;
   
-      while(regen){ 
+      while(regen && maxTries > 0){ 
         const intNumber = (Math.floor(Math.random() * (999999999 - 100000000 + 1)) + 100000000);
         const stringNumber = intNumber.toString();
         phoneNumber = "07"+reverseString(stringNumber);
   
         //Check if phone number has already been generated
-        regen = phoneNumberList.includes(phoneNumber)
+        regen = phoneNumberList.includes(phoneNumber);
+        maxTries--;
+
       }
   
       phoneNumberList.push(phoneNumber);
