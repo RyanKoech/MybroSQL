3b47c1f8c91ed870e0c2f6ec10a1e23a6da509cb
RyanKoech <sirryankoech@gmail.com>
2022-06-18 09:11:34 +0300

    Added support for generating unique data with inifity loop safety

--- src/generators/address/address-generator.jsx
+++ src/generators/address/address-generator.jsx
@@ -1,62 +1,158 @@
 import {faker} from '@faker-js/faker'
 
 const addressGenerator = {
-  address :  (rowCount) => {
+  address :  (rowCount, isUnique) => {
 
     const addressList = [];
+
+    
+    if(isUnique){
+
+      for(let i = 0; i < rowCount; i++){
+
+        let streetAddress = "";
+        let city = "";
+        let state = "";
+        let address = "";
+        let maxTries = 10;
+        let regen = true;
+
+        while(regen && maxTries > 0){
+
+          streetAddress = faker.address.streetAddress();
+          city = faker.address.city();
+          state = faker.address.stateAbbr();
+      
+          address = `${streetAddress}, ${city}, ${state}`;
+          regen = addressList.includes(address);
+          maxTries--;
+          
+        }
+        addressList.push(address);
+
+      }
+    }else{
+
+      for(let i = 0; i < rowCount; i++){
   
-    for(let i = 0; i < rowCount; i++){
-  
-      const stressAddress = faker.address.streetAddress();
-      const city = faker.address.city();
-      const state = faker.address.stateAbbr();
-  
-      const address = `${stressAddress}, ${city}, ${state}`;
-  
-      addressList.push(address);
+        const streetAddress = faker.address.streetAddress();
+        const city = faker.address.city();
+        const state = faker.address.stateAbbr();
+    
+        const address = `${streetAddress}, ${city}, ${state}`;
+    
+        addressList.push(address);
+      }
     }
   
     return addressList;
   }, 
 
-  city: (rowCount) => {
+  city: (rowCount, isUnique) => {
 
     const cityList = [];
   
-    for(let i = 0; i < rowCount; i++){
+
+    if(isUnique){
+
+      for(let i = 0; i < rowCount; i++){
+
+        let city = "";
+        let maxTries = 10;
+        let regen = true;
+
+        while(regen && maxTries > 0){
+
+          city = faker.address.cityName();
+          regen = cityList.includes(city);
+          maxTries--;
+          
+        }
+
+        cityList.push(city);
+
+      }
+    }else{
+
+      for(let i = 0; i < rowCount; i++){
   
-      const city = faker.address.cityName();
-  
-      cityList.push(city);
+        const city = faker.address.cityName();
+        cityList.push(city);
+
+      }
     }
-  
+
     return cityList;
   },
 
-  country: (rowCount) => {
+  country: (rowCount, isUnique) => {
 
     const countryList = [];
+
+    if(isUnique){
+
+      for(let i = 0; i < rowCount; i++){
+
+        let country = "";
+        let maxTries = 10;
+        let regen = true;
+
+        while(regen && maxTries > 0){
+
+          country = faker.address.country();
+          regen = countryList.includes(country);
+          maxTries--;
+          
+        }
+        countryList.push(country);
+
+      }
+    }else{
+
+      for(let i = 0; i < rowCount; i++){
   
-    for(let i = 0; i < rowCount; i++){
-  
-      const country = faker.address.country();
-  
-      countryList.push(country);
+        const country = faker.address.country();
+    
+        countryList.push(country);
+      }
     }
   
     return countryList;
   },
 
-  state: (rowCount) => {
+  state: (rowCount, isUnique) => {
 
     const stateList = [];
+
+    
+    if(isUnique){
+
+      for(let i = 0; i < rowCount; i++){
+
+        let state = "";
+        let maxTries = 10;
+        let regen = true;
+
+        while(regen && maxTries > 0){
+
+          state = faker.address.state();
+          regen = stateList.includes(state);
+          maxTries--;
+          
+        }
+        stateList.push(state);
+
+      }
+    }else{
+
+      for(let i = 0; i < rowCount; i++){
   
-    for(let i = 0; i < rowCount; i++){
-  
-      const state = faker.address.state();
-  
-      stateList.push(state);
+        const state = faker.address.state();
+    
+        stateList.push(state);
+      }
     }
+
   
     return stateList;
   },
@@ -71,12 +167,14 @@ const addressGenerator = {
       for(let i = 0; i < rowCount; i++){
 
         let postalCode = "";
+        let maxTries = 10;
         let regen = true;
 
-        while(regen){
+        while(regen && maxTries > 0){
 
           postalCode = faker.address.zipCode('#####-#####');
           regen = postalCodeList.includes(postalCode);
+          maxTries--;
 
         }
 
