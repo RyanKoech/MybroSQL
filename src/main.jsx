cdcc7bc65b10e24cf3bc3318026852e0f659de46
RyanKoech <sirryankoech@gmail.com>
2022-06-18 13:52:39 +0300

    Added Context Provider Above App Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
+//React Imports
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+//External Libraries Imports
 import 'flowbite';
+
+//Components Imports
+import App from './App'
+
+//App Context
+import AppContextProvider from './context/AppContext';
+
+//Styles Imports
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </React.StrictMode>
 )
