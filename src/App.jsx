6299880de1dd7ba219f13f7352d61344536e7174
RyanKoech <sirryankoech@gmail.com>
2022-06-19 13:59:06 +0300

    Added Component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import MainApp from "./components/MainApp";
 import Result from './components/Result';
+import ScrollToTop from "./components/ScrollToTop";
 
 //App Context
 import { AppContext } from "./context/AppContext";
@@ -25,6 +26,7 @@ function App() {
           : (
             <MainApp/>)}
         </div>
+        <ScrollToTop/>
       </div>
   );
 }
