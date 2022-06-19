7aba664e0f18302f038f5ba18a46f4ba184cdfdb
RyanKoech <sirryankoech@gmail.com>
2022-06-19 17:24:30 +0300

    Reformatted Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //React Imports
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 
 //Style Imports
 import "./App.css";
@@ -7,27 +7,26 @@ import "./App.css";
 //Components Imports
 import Navbar from "./components/Navbar";
 import MainApp from "./components/MainApp";
-import Result from './components/Result';
+import Result from "./components/Result";
 import ScrollToTop from "./components/ScrollToTop";
 
 //App Context
 import { AppContext } from "./context/AppContext";
 
 function App() {
-
-  const { showResults} = useContext(AppContext);
+  const { showResults } = useContext(AppContext);
 
   return (
-      <div className="bg-white dark:bg-slate-900 pb-10">
-        <div className="container min-h-screen mx-auto w-full md:pt-4">
+    <div className="bg-white dark:bg-slate-900 pb-10">
+      <div className="container min-h-screen mx-auto w-full md:pt-4">
           <Navbar/>
           {showResults
           ? (<Result/>) 
           : (
             <MainApp/>)}
         </div>
-        <ScrollToTop/>
-      </div>
+      <ScrollToTop />
+    </div>
   );
 }
 
