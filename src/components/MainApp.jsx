4cd13929c5d32ec8fcb859e36a4f041c70d21c2e
RyanKoech <sirryankoech@gmail.com>
2022-06-19 12:33:47 +0300

    Fixed Element Attribute

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -75,7 +75,7 @@ const MainApp = React.memo(() => {
           </h2>
           <button
             type="button"
-            class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             onClick={handleOnClickReset}
           >
             Reset
