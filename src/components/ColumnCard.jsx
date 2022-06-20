277aecdb82153b4e48c98e6f5b72f673d4a3fce3
RyanKoech <sirryankoech@gmail.com>
2022-06-20 11:17:56 +0300

    Added tooltip icon

--- src/components/ColumnCard.jsx
+++ src/components/ColumnCard.jsx
@@ -150,7 +150,26 @@ const ColumnCard = React.memo(({ colName, colIndex }) => {
             />
             <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Unique
+              Unique&nbsp;
+            </span>
+            <span
+              className="inline cursor-pointer"
+              title="Use this both for the PRIMARY KEY and UNIQUE KEY attributes."
+            >
+              <svg
+                className="w-4 h-4 text-gray-600 dark:text-slate-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
             </span>
           </label>
         </div>
